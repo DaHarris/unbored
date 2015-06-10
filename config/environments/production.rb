3098bbdb879a5c44ac8012b12915dce3a66030de
Daniel Harris <bobth3bum@gmail.com>
2015-06-10 16:44:45 -0600

    change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -76,4 +78,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.compile = true
+
 end
