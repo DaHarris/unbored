c5799dd58792633246a78ebe5a3e08594dc774ef
Daniel Harris <bobth3bum@gmail.com>
2015-06-10 23:37:02 -0600

    add search bar

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -27,6 +27,51 @@ $(document).ready(function() {
         var activityAsked = false;
         var allMarkers = [];
 
+
+        var input = document.getElementById('pac-input')
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var searchBox = new google.maps.places.SearchBox(input);
+
+        //Listen for the event fired when the user selects an item from the
+        //pick list. Retrieve the matching places for that item.
+        google.maps.event.addListener(searchBox, 'places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          // if (places.length == 0) {
+          //   return;
+          // }
+          // for (var i = 0, marker; marker = markers[i]; i++) {
+          //   marker.setMap(null);
+          // }
+
+          // For each place, get the icon, place name, and location.
+          queryMarkers = [];
+          var bounds = new google.maps.LatLngBounds();
+          for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+              map: map,
+              icon: image,
+              title: place.name,
+              position: place.geometry.location
+            });
+
+            queryMarkers.push(marker);
+
+            bounds.extend(place.geometry.location);
+          }
+          map.fitBounds(bounds);
+          map.setZoom(14);
+        });
+
         function clearMarkers() {
           allMarkers = markers;
           for (i=0;i<markers.length;i++) {
